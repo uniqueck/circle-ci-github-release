97b3d8348163503de514e080b22562633c2ea4f9
Constantin Krüger <Constantinidis@web.de>
2019-06-01 23:52:46 +0200

    initial commit

---
+++ src/main/java/de/ckr/software/circlecigithubrelease/CircleCiGithubReleaseApplication.java
@@ -0,0 +1,13 @@
+package de.ckr.software.circlecigithubrelease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleCiGithubReleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircleCiGithubReleaseApplication.class, args);
+	}
+
+}
