ae8a5ad438e968b1c76341431c7fae9ecd8eb757
Constantin Krüger <Constantinidis@web.de>
2019-06-04 23:38:52 +0200

    modify scirpt to send link for download

--- scripts/telegram_notification.sh
+++ scripts/telegram_notification.sh
@@ -1,27 +1,20 @@
 #!/bin/sh
 
-# Get the token from Travis environment vars and build the bot URL:
 BOT_URL="https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage"
 
-# Set formatting for the message. Can be either "Markdown" or "HTML"
+GITHUB_RELEASE_URL="https://github.com/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/releases/tag/${CIRCLE_TAG}"
+
 PARSE_MODE="Markdown"
 
-BUILD_STATUS=$1
-
-# Define send message function. parse_mode can be changed to
-# HTML, depending on how you want to format your message:
 send_msg () {
     curl -s -X POST ${BOT_URL} -d chat_id=${TELEGRAM_CHAT_ID} \
         -d text="$1" -d parse_mode=${PARSE_MODE}
 }
 
 
-# Send message to the bot with some pertinent details about the job
-# Note that for Markdown, you need to escape any backtick (inline-code)
-# characters, since they're reserved in bash
 send_msg "
 -------------------------------------
-CircleCI build *${BUILD_STATUS}!*
-\`Repository:  ${CIRCLE_REPOSITORY_URL}\`
+Release: *${CIRCLE_TAG}*
+[Download Release Artifacts here](${GITHUB_RELEASE_URL})
 --------------------------------------
 "
